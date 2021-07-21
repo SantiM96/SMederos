a4ed2c44e58ef8d9c65df2011788825783eda5bf
SantiM96 <smt093@gmail.com>
2021-07-21 20:02:52 -0300

    add auto closes in the bar and fix some bugs

--- public/js/main.js
+++ public/js/main.js
@@ -1,80 +1,109 @@
-// slider
-const webs = document.getElementById("webs"),
-    totalWebs = document.querySelectorAll(".webToSlider"),
-    thumbnailTotal = document.querySelectorAll(".thumbnail"),
-    thumbnailContainer = document.querySelector("#thumbnailContainer"),
-    setTimeInterval = 5000
-let sliderAutomatic = setInterval(slider, setTimeInterval),
-    state = false,
-    count = 0
+$(document).ready(function () {
+    // slider //
+    const webs = document.getElementById("webs"),
+        totalWebs = document.querySelectorAll(".webToSlider"),
+        thumbnailTotal = document.querySelectorAll(".thumbnail"),
+        thumbnailContainer = document.querySelector("#thumbnailContainer"),
+        setTimeInterval = 5000
 
-function slider() {
-    count++
-    if (count == totalWebs.length) count = 0
-    selectCard(totalWebs)
-}
+    let sliderAutomatic = setInterval(slider, setTimeInterval),
+        state = false,
+        count = 0
 
-function selectCard(totalElements, nImg = count) {
-    for (position of totalElements) {
-        if (position == totalElements[nImg]) {
-            position.classList.replace("d-none", "d-block")
-
-            document.getElementsByClassName("activeBar")[0].classList.remove("activeBar")
-            thumbnailTotal[count].classList.add("activeBar")
-        }
-        else position.classList.replace("d-block", "d-none")
+    function slider() {
+        count++
+        if (count == totalWebs.length) count = 0
+        selectCard(totalWebs)
     }
-}
 
-// Delegation for thumbnail
-thumbnailContainer.addEventListener('click', e => {
-    if (e.target == thumbnailContainer) return
-    let thumbClicked = parseInt(e.target.getAttribute('data-number'))
-    selectCard(totalWebs, thumbClicked)
-    document.getElementsByClassName("activeBar")[0].classList.remove("activeBar")
-    thumbnailTotal[thumbClicked].classList.add("activeBar")
-    count = thumbClicked
-    clearInterval(sliderAutomatic)
-    sliderAutomatic = setInterval(slider, setTimeInterval)
-})
+    function selectCard(totalElements, nImg = count) {
+        for (position of totalElements) {
+            if (position == totalElements[nImg]) {
+                position.classList.replace("d-none", "d-block")
 
-// keep while the click is over webs and restore interval after leave the element
-webs.addEventListener('mouseover', () => clearInterval(sliderAutomatic))
-webs.addEventListener('mouseout', () => sliderAutomatic = setInterval(slider, setTimeInterval))
-// finish slider
+                document.getElementsByClassName("activeBar")[0].classList.remove("activeBar")
+                thumbnailTotal[count].classList.add("activeBar")
+            } else position.classList.replace("d-block", "d-none")
+        }
+    }
 
-// navbar
+    // Delegation for thumbnail
+    thumbnailContainer.addEventListener('click', e => {
+        if (e.target == thumbnailContainer) return
+        let thumbClicked = parseInt(e.target.getAttribute('data-number'))
+        selectCard(totalWebs, thumbClicked)
+        document.getElementsByClassName("activeBar")[0].classList.remove("activeBar")
+        thumbnailTotal[thumbClicked].classList.add("activeBar")
+        count = thumbClicked
+        clearInterval(sliderAutomatic)
+        sliderAutomatic = setInterval(slider, setTimeInterval)
+    })
 
-const body = $("body"),
-    navbar = $("#auto-add-active"),
-    home = $("#home"),
-    parentNav = $("#myName")
+    // keep while the click is over webs and restore interval after leave the element
+    webs.addEventListener('mouseover', () => clearInterval(sliderAutomatic))
+    webs.addEventListener('mouseout', () => sliderAutomatic = setInterval(slider, setTimeInterval))
+    // finish slider //
+
+
+
+    // navbar //
+    const body = $("body"),
+        navbar = $("#auto-add-active"),
+        home = $("#home"),
+        parentNav = $("#myName")
+
+    let heightToChange = home.height() / 2 + parentNav.height() / 2,
+        change = true
+    
+    // move to top side
+    window.onscroll = () => {
+        if (heightToChange <= window.scrollY && change) {
+            navbar.remove()
+            home.before(navbar)
+            heightToChange = home.height() / 2 + parentNav.height() * 1.02
+            change = false
+            navbar
+                .removeClass('gradient-nav')
+                .removeClass('animate__pulse')
+                .addClass('animate__fadeInDown')
+        }
+        else if (heightToChange >= window.scrollY && !change) {
+            navbar.remove()
+            parentNav.append(navbar)
+            heightToChange = home.height() / 2 + parentNav.height() / 2
+            change = true
+            navbar
+                .addClass('gradient-nav')
+                .removeClass('animate__fadeInDown')
+                .addClass('animate__pulse')
+        }
+        // add color to home
+        if (!(document.querySelector('.active'))) {
+            document.querySelector('#home-btn').classList.add('active')
+        }
+    }
+
+    // add color to home initial
+    if (!(document.querySelector('.active'))) {
+        document.querySelector('#home-btn').classList.add('active')
+    }
+
+    // Delegation for auto close in movile version
+    if (window.screen.width < 992) {
+        let menuBtn = document.querySelector('#menu-btn')
+        document.querySelector('#auto-add-active').addEventListener('click', () => {
+            menuBtn.click()
+        })
+        document.querySelector('body').addEventListener('click', () => {
+            if ($('#navbarSupportedContent').hasClass('show')) {
+                $('#navbarSupportedContent').removeClass('show')
+            }
+        })
+    }
+    // finish navbar //
+
+
+
     
 
-
-let heightToChange = home.height() / 2 + parentNav.height() / 2,
-    change = true
-
-window.onscroll = () => {
-    if (heightToChange <= window.scrollY && change) {
-        navbar.remove()
-        home.before(navbar)
-        heightToChange = home.height() / 2 + parentNav.height() * 1.06
-        change = false
-        navbar
-            .removeClass('gradient-nav')
-            .removeClass('animate__pulse')
-            .addClass('animate__fadeInDown')
-        
-    }
-    else if (heightToChange >= window.scrollY && !change) {
-        navbar.remove()
-        parentNav.append(navbar)
-        heightToChange = home.height() / 2 + parentNav.height() / 2
-        change = true
-        navbar
-            .addClass('gradient-nav')
-            .removeClass('animate__fadeInDown')
-            .addClass('animate__pulse')
-    }
-}
+});
